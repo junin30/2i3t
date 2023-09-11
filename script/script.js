c90fb0922646de84941ed51acd964f3cea782145
João Pedro <106189730+joaosilva2007@users.noreply.github.com>
2023-09-11 10:50:21 -0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,3 +1,16 @@
-function alo_mundo(){
-    alert("Projeto Iniciado! Alô!") 
+escrever = msg => alert(msg);
+soma = (a,b) => a + b;
+sub = (a,b) => a - b;
+mult = (a,b) => a * b;
+div = (a,b) => a / b;
+raiz = a => Math.sqrt(a);
+equacao2Grau = (a,b,c) =>{
+    let delta =  sub(mult(b,b),mult(4,mult(a,c)));
+    if(delta < 0 ) return "Não possui Raiz Real.";
+    if(delta == 0 ) return "X1 = X2 =" + div(-b,mult(2,a));
+    return "X1 = " + div(soma(-b,raiz(delta)),mult(2,a)) + " X2 = "
+    + div(sub(-b,raiz(delta)),mult(2,a));
 }
+
+
+escrever(equacao2Grau(3,-2,-1))
